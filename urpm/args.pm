e97552ae310209e2a500b3dc56890ad35f787d62
Thierry Vignaud <thierry.vignaud@gmail.com>
2016-10-15 00:34:31 +0200

    fix more Getopt::Long breakages
    now testuite pass again after droping the "gnu_compat" option

--- urpm/args.pm
+++ urpm/args.pm
@@ -18,7 +18,7 @@ our @EXPORT = '%options';
 
 # Configuration of Getopt. urpmf is a special case, because we need to
 # parse non-alphanumerical options (-! -( -))
-my @configuration = qw(bundling gnu_compat no_ignore_case permute);
+my @configuration = qw(bundling no_ignore_case permute);
 push @configuration, 'pass_through'
     if $tool eq 'urpmf' || $tool eq 'urpmi.addmedia';
 Getopt::Long::Configure(@configuration);
