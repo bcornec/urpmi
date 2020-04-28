0c75fab9fad1c9e63962c0c853eaf108bca5db9e
Thierry Vignaud <thierry.vignaud@gmail.com>
2020-04-28 14:43:21 +0200

    disable fsync/fdatasync while running tests

--- t/helper.pm
+++ t/helper.pm
@@ -39,6 +39,7 @@ sub need_root_and_prepare() {
     system('rm -rf root');
     isnt(-d 'root', "test root dir can not be removed $!", "creating chroot");
     system('mkdir -p root/etc/rpm');
+    system('echo "%__dbi_other fsync nofsync" >root/etc/rpm/macros');
     system('echo "%_pkgverify_level none" >root/etc/rpm/macros');
     $using_root = 1;
     $ENV{LC_ALL} = 'C';
